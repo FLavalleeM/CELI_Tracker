2fbd4f41512a72f63b7e1f71552177f606e3ed96
FLavalleeM <152932927+FLavalleeM@users.noreply.github.com>
2025-03-05 13:43:41 -0500

    .

--- celi_streamlit.py
+++ celi_streamlit.py
@@ -499,7 +499,7 @@ elif page == "Cumulative Return":
         fig_pie.update_traces(textinfo='percent+label')
         fig_pie.update_layout(
             width=1000,
-            height=800,
+            height=400,
             showlegend=True
         )
 
